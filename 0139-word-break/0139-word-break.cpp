e0167a92f1888c99d1c903342fadb14bc6933ee4
Seeunggyu Park <57701607+powerpower2005@users.noreply.github.com>
2022-10-13 17:33:42 +0900

    Time: 0 ms (100.00%), Space: 7.8 MB (86.29%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    //dp[i] : dp[i]부터 끝까지 단어로 쪼갤 수 있느냐? 1 : -1
+    //dp[0] : 문자열 전부를 끝까지 단어로 쪼갤 수 있느냐?
+    
+    
+    int x;
+    int check(string &s, int idx, vector<string> &dict, vector<int> &dp){
+        
+        if(idx == x){
+            return 1;
+        }
+        
+        int &result = dp[idx];
+
+        if(result != -1){
+            return result;
+        }
+        result = 0;
+        for(auto w : dict){
+            int wSize = w.size();
+            if(idx + wSize > x)continue;
+            string comp = s.substr(idx, wSize);
+            if(comp == w){
+                result |= check(s, idx + wSize, dict, dp);  //하나라도 1이면 가능
+                if(result == 1)return result;
+            }
+        }
+        
+        return result;
+        
+    }
+    
+    
+    bool wordBreak(string s, vector<string>& wordDict) {
+        x = s.size();
+        vector<int> dp(x+1,-1);
+        
+        return check(s, 0, wordDict, dp) == 1;
+    }
+};
